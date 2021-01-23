38cafe14a173971ee06083d2eaf93730eea44819
João Carlos Pandolfi <joaopandolfi@gmail.com>
2021-01-23 18:25:29 -0300

    gracefull shutdown

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/joaopandolfi/blackwhale/configurations"
 	"github.com/joaopandolfi/go_generic_api/config"
 	"github.com/joaopandolfi/go_generic_api/routes"
 
+	"github.com/joaopandolfi/blackwhale/remotes/mongo"
 	"github.com/joaopandolfi/blackwhale/remotes/mysql"
 	"github.com/joaopandolfi/blackwhale/utils"
 )
@@ -29,8 +35,23 @@ func resilient() {
 	}
 }
 
+func gracefullShutdown() {
+	mysql.Close()
+	mongo.Close()
+}
+
+func welcome() {
+	// https://patorjk.com/software/taag/#p=display&f=Slant&t=ms%20-%20calendar
+	fmt.Println("#####################")
+	fmt.Println("#  go_generic_api!  #")
+	fmt.Println("#####################")
+	fmt.Println("")
+}
+
 func main() {
 	defer resilient()
+
+	welcome()
 
 	//Init
 	configInit()
@@ -40,6 +61,7 @@ func main() {
 
 	// Security
 	routes.Handlers(r)
+	r.Use(mux.CORSMethodMiddleware(r))
 
 	// Routes consist of a path and a handler function.
 	routes.Register(r)
@@ -56,14 +78,28 @@ func main() {
 		ReadTimeout:  configurations.Configuration.Timeout.Read,
 	}
 
-	var err error
-	if config.Config.Debug {
-		err = srv.ListenAndServe()
-	} else {
-		err = srv.ListenAndServeTLS(config.Config.TLSCert, config.Config.TLSKey)
-	}
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
-	if err != nil {
-		utils.CriticalError("Fatal server error", err.Error())
+	go func() {
+		var err error
+		if config.Config.Debug {
+			err = srv.ListenAndServe()
+		} else {
+			err = srv.ListenAndServeTLS(config.Config.TLSCert, config.Config.TLSKey)
+		}
+		if err != nil && err != http.ErrServerClosed {
+			utils.CriticalError("Fatal server error", err.Error())
+		}
+	}()
+
+	<-done
+	utils.Info("[SERVER] Gracefully shutdown")
+	gracefullShutdown()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	if err := srv.Shutdown(ctx); err != nil {
+		utils.CriticalError("Server Shutdown Failed", err.Error())
 	}
+	cancel()
 }
